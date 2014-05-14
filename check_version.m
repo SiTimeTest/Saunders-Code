ac812a8213ec8290dbe16af998d9245afbd34cd8
SiTime Test <jyu@sitime.com>
2014-05-14 16:51:15 -0700

    test automated script 140514165115

--- check_version.m
+++ check_version.m
@@ -25,7 +25,7 @@ result = git(['checkout -b ',runinfo.operator]);
 if strcmp(result(1:5),'fatal')
 %     fprintf('Branch already exists\n\n');
     result = git(['checkout ',runinfo.operator])
-    if (strcmp(result(1:5), 'error'))
+    if (strcmp(result(1:5),  'error'))
         need_to_update = 1;
     end
 else
